e155f1ac10a5acb68b7fb6915cf41a8d1a4a36a4
Cho0 <choojason7@gmail.com>
2022-01-18 12:33:27 -0800

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,17 +3,21 @@ import React, { Component } from "react";
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 16,
+    message: ""
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    if (value < 1 || value > 16) {
+    if (value < 0 || value > 16) {
       this.setState({
-        numberOfEvents: '',
+        message: "Enter number between 1-16"
+      })
+    } else {
+      this.setState({
+        numberOfEvents: value,
       })
     }
-
-  };
+  }
 
   render() {
     return (
